d7e65edf4945a92a60a3e89ccbe7f29c919ccd96
AdverseGecko3 <eric.barrero@hotmail.com>
2022-03-09 15:50:44 +0100

    - Fixed a bug for [old] tokens, where the app crashed as its data is "chaotic" and API returns empty data. Now a "No data" text will appear. - Optimized DialogCryptoDetail code.

--- app/src/main/java/com/kotlin/cryptofication/ui/view/DialogCryptoDetail.kt
+++ app/src/main/java/com/kotlin/cryptofication/ui/view/DialogCryptoDetail.kt
@@ -40,90 +40,107 @@ class DialogCryptoDetail : BottomSheetDialogFragment(), OnChartValueSelectedList
 
         val selectedCrypto: Crypto? = arguments?.getParcelable("selectedCrypto")
 
-        // Set the data to TextViews
-        binding.tvFragmentCryptoDetailName.text = selectedCrypto!!.name
-        binding.tvFragmentCryptoDetailSymbol.text = selectedCrypto.symbol!!.uppercase()
-        binding.tvFragmentCryptoDetailRank.text =
-            "#${selectedCrypto.market_cap_rank}"
-        binding.tvFragmentCryptoDetailPriceChangePercentage24h.text =
-            selectedCrypto.price_change_percentage_24h.customFormattedPercentage()
-        if (selectedCrypto.price_change_percentage_24h >= 0) {
-            binding.tvFragmentCryptoDetailPriceChangePercentage24h.positivePrice()
-        } else {
-            binding.tvFragmentCryptoDetailPriceChangePercentage24h.negativePrice()
-        }
-        binding.tvFragmentCryptoDetailPriceChange24h.text = run {
-            if (selectedCrypto.price_change_24h >= 0) {
-                binding.tvFragmentCryptoDetailPriceChange24h.positivePrice()
-                "+${selectedCrypto.price_change_24h.customFormattedPrice(userCurrency)}"
+        binding.apply {
+            // Set the data to TextViews
+            tvFragmentCryptoDetailName.text = selectedCrypto!!.name
+            tvFragmentCryptoDetailSymbol.text = selectedCrypto.symbol!!.uppercase()
+            tvFragmentCryptoDetailRank.text =
+                "#${selectedCrypto.market_cap_rank}"
+            tvFragmentCryptoDetailPriceChangePercentage24h.text =
+                selectedCrypto.price_change_percentage_24h.customFormattedPercentage()
+            if (selectedCrypto.price_change_percentage_24h >= 0) {
+                tvFragmentCryptoDetailPriceChangePercentage24h.positivePrice()
             } else {
-                binding.tvFragmentCryptoDetailPriceChange24h.negativePrice()
-                selectedCrypto.price_change_24h.customFormattedPrice(userCurrency)
+                tvFragmentCryptoDetailPriceChangePercentage24h.negativePrice()
             }
+            tvFragmentCryptoDetailPriceChange24h.text = run {
+                if (selectedCrypto.price_change_24h >= 0) {
+                    tvFragmentCryptoDetailPriceChange24h.positivePrice()
+                    "+${selectedCrypto.price_change_24h.customFormattedPrice(userCurrency)}"
+                } else {
+                    tvFragmentCryptoDetailPriceChange24h.negativePrice()
+                    selectedCrypto.price_change_24h.customFormattedPrice(userCurrency)
+                }
+            }
+            tvFragmentCryptoDetailPrice.text =
+                selectedCrypto.current_price.customFormattedPrice(userCurrency)
         }
-        binding.tvFragmentCryptoDetailPrice.text =
-            selectedCrypto.current_price.customFormattedPrice(userCurrency)
 
         val lineData = arrayListOf<Entry>()
-        for (i in selectedCrypto.sparkline_in_7d!!.price.indices) {
+        for (i in selectedCrypto!!.sparkline_in_7d?.price!!.indices) {
             lineData.add(Entry(i.toFloat(), selectedCrypto.sparkline_in_7d!!.price[i].toFloat()))
         }
 
-        val lds = LineDataSet(lineData, "LineData")
-        // Change chart line color
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            lds.color = mResources.getColor(R.color.purple_app_accent, null)
+        if (lineData.isNullOrEmpty()) {
+            binding.apply {
+                bcFragmentCryptoDetailLine.visibility = View.GONE
+                tvFragmentCryptoDetailPriceHigh7d.visibility = View.GONE
+                tvFragmentCryptoDetailPriceLow7d.visibility = View.GONE
+                tvFragmentCryptoDetailNoData.visibility = View.VISIBLE
+            }
         } else {
-            lds.color = mResources.getColor(R.color.purple_app_accent)
-        }
-        // Change chart line width
-        lds.lineWidth = 2F
-        // Remove chart circles
-        lds.setDrawCircles(false)
-        lds.setDrawFilled(true)
-        lds.fillDrawable =
-            ContextCompat.getDrawable(requireContext(), R.drawable.gradient_background_chart)
+            val lds = LineDataSet(lineData, "LineData").apply {
+                color = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    mResources.getColor(R.color.purple_app_accent, null)
+                } else {
+                    mResources.getColor(R.color.purple_app_accent)
+                }
+                // Change chart line width
+                lineWidth = 2F
+                // Remove chart circles
+                setDrawCircles(false)
+                setDrawFilled(true)
+                fillDrawable =
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.gradient_background_chart
+                    )
+            }
 
-        val ld = LineData(arrayListOf(lds) as List<ILineDataSet>?)
-        binding.bcFragmentCryptoDetailLine.data = ld
-        // Remove legend
-        binding.bcFragmentCryptoDetailLine.legend.isEnabled = false
-        // Remove description
-        binding.bcFragmentCryptoDetailLine.description.isEnabled = false
-        // Remove axis left and right legend
-        binding.bcFragmentCryptoDetailLine.axisLeft.isEnabled = false
-        binding.bcFragmentCryptoDetailLine.axisRight.isEnabled = false
-        // Remove xAxis chart-lines
-        binding.bcFragmentCryptoDetailLine.xAxis.isEnabled = false
-        // Animate chart when loads
-        binding.bcFragmentCryptoDetailLine.animateX(375)
-        // Disable double tap to zoom
-        binding.bcFragmentCryptoDetailLine.isDoubleTapToZoomEnabled = false
-        // Disable both axis scale
-        binding.bcFragmentCryptoDetailLine.isScaleYEnabled = false
-        binding.bcFragmentCryptoDetailLine.isScaleXEnabled = false
-        // Change chart offsets
-        binding.bcFragmentCryptoDetailLine.setViewPortOffsets(25F, 0F, 25F, 50F)
-        // Max zoom
-        binding.bcFragmentCryptoDetailLine.viewPortHandler.setMaximumScaleX(3F)
-        binding.bcFragmentCryptoDetailLine.setOnChartValueSelectedListener(this)
-        binding.bcFragmentCryptoDetailLine.setOnTouchListener { _, _ ->
-            binding.bcFragmentCryptoDetailLine.onTouchListener.setLastHighlighted(null)
-            binding.bcFragmentCryptoDetailLine.highlightValue(null)
-            binding.tvFragmentCryptoDetailPrice.text =
-                selectedCrypto.current_price.customFormattedPrice(userCurrency)
-            val bottomSheetDialog = dialog as BottomSheetDialog
-            val bottomSheetBehavior = bottomSheetDialog.behavior
-            bottomSheetBehavior.isDraggable = true
-            false
-        }
+            val ld = LineData(arrayListOf(lds) as List<ILineDataSet>?)
+            binding.apply {
+                bcFragmentCryptoDetailLine.apply {
+                    data = ld
+                    legend.isEnabled = false
+                    // Remove description
+                    description.isEnabled = false
+                    // Remove axis left and right legend
+                    axisLeft.isEnabled = false
+                    axisRight.isEnabled = false
+                    // Remove xAxis chart-lines
+                    xAxis.isEnabled = false
+                    // Animate chart when loads
+                    animateX(375)
+                    // Disable double tap to zoom
+                    isDoubleTapToZoomEnabled = false
+                    // Disable both axis scale
+                    isScaleYEnabled = false
+                    isScaleXEnabled = false
+                    // Change chart offsets
+                    setViewPortOffsets(25F, 0F, 25F, 50F)
+                    // Max zoom
+                    viewPortHandler.setMaximumScaleX(3F)
+                    setOnChartValueSelectedListener(this@DialogCryptoDetail)
+                    setOnTouchListener { _, _ ->
+                        onTouchListener.setLastHighlighted(null)
+                        highlightValue(null)
+                        tvFragmentCryptoDetailPrice.text =
+                            selectedCrypto.current_price.customFormattedPrice(userCurrency)
+                        val bottomSheetDialog = dialog as BottomSheetDialog
+                        val bottomSheetBehavior = bottomSheetDialog.behavior
+                        bottomSheetBehavior.isDraggable = true
+                        false
+                    }
+                }
 
-        binding.tvFragmentCryptoDetailPriceHigh7d.text =
-            selectedCrypto.sparkline_in_7d!!.price.maxOrNull()!!
-                .customFormattedPrice(userCurrency)
-        binding.tvFragmentCryptoDetailPriceLow7d.text =
-            selectedCrypto.sparkline_in_7d!!.price.minOrNull()!!
-                .customFormattedPrice(userCurrency)
+                selectedCrypto.sparkline_in_7d?.price.also {
+                    tvFragmentCryptoDetailPriceHigh7d.text =
+                        it?.maxOrNull()?.customFormattedPrice(userCurrency)
+                    tvFragmentCryptoDetailPriceLow7d.text =
+                        it?.minOrNull()?.customFormattedPrice(userCurrency)
+                }
+            }
+        }
 
         return binding.root
     }
